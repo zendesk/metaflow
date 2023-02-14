d3c6f985dfb66fc761dfdc6a65c6bee2721102d0
Jeremy Kirkwood <30310194+jkirkwoodzendesk@users.noreply.github.com>
2023-02-14 17:03:37 +1100

    Minimal example

---
+++ metaflow-sandbox/minimal.tf
@@ -0,0 +1,92 @@
+###############################################################################
+# An example using this module to set up a minimal deployment Metaflow
+# with AWS Batch support, without the UI.
+###############################################################################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3"
+    }
+  }
+}
+
+locals {
+  account_id        = "794874039740"
+  environment_tag   = "sandbox"
+  region            = "ap-southeast-2"
+}
+
+provider "aws" {
+  region              = local.region
+  allowed_account_ids = [local.account_id]
+  assume_role {
+    role_arn = "arn:aws:iam::${local.account_id}:role/atlantis-assume-roles/atlantis-role-trisolaris"
+  }
+  default_tags {
+    tags = {
+      Name = "" # see https://github.com/hashicorp/terraform-provider-aws/issues/19583
+      team = "ml-apac-trisolaris"
+      "final-expiration-date" = "2023-03-15"
+    }
+  }
+}
+
+# Random suffix for this deployment
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+locals {
+  resource_prefix = "metaflow"
+  resource_suffix = random_string.suffix.result
+}
+
+data "aws_availability_zones" "available" {
+}
+
+# VPC infra using https://github.com/terraform-aws-modules/terraform-aws-vpc
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.13.0"
+
+  name = "${local.resource_prefix}-${local.resource_suffix}"
+  cidr = "10.10.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.10.8.0/21", "10.10.16.0/21", "10.10.24.0/21"]
+  public_subnets  = ["10.10.128.0/21", "10.10.136.0/21", "10.10.144.0/21"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+}
+
+
+module "metaflow" {
+  source  = "outerbounds/metaflow/aws"
+  version = "0.8.0"
+
+  resource_prefix = local.resource_prefix
+  resource_suffix = local.resource_suffix
+
+  enable_step_functions = false
+  subnet1_id            = module.vpc.public_subnets[0]
+  subnet2_id            = module.vpc.public_subnets[1]
+  vpc_cidr_blocks       = [module.vpc.vpc_cidr_block]
+  vpc_id                = module.vpc.vpc_id
+  with_public_ip        = "false"
+
+  tags = {
+    "managedBy" = "terraform"
+  }
+}
+
+# The module will generate a Metaflow config in JSON format, write it to a file
+resource "local_file" "metaflow_config" {
+  content  = module.metaflow.metaflow_profile_json
+  filename = "./metaflow_profile.json"
+}
